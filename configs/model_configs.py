e15fa04070639cb613abf45a681fbf61dbcbe0a9
wills <willsgao@163.com>
2026-05-15 11:07:56 +0800

    更新页面显示

--- configs/model_configs.py
+++ configs/model_configs.py
@@ -1,4 +1,5 @@
 # 选择模型
-model = "deepseek-v4-flash"
+model = "deepseek-chat"
+# model = "deepseek-v4-flash"
 # 达到Rate limit时的备选模型
 backup_models = ["deepseek-v4-flash", "Pro/deepseek-ai/DeepSeek-V3", "deepseek-v3", "Qwen/Qwen2.5-72B-Instruct"]
